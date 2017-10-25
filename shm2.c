e898795194809e1c39924c38f653c67adc8cea09
Shantanu Salil <shantanu.salil123@gmail.com>
2017-10-25 20:29:01 +0530

    Update shm2.c

--- shm2.c
+++ shm2.c
@@ -5,7 +5,12 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include "shm_com.h"
+
+#define TEXT_SZ 2048
+struct shared_use_st{
+	int written_by_you;
+	char some_text[TEXT_SZ];
+};
 int main()
 {
 	int running = 1;
